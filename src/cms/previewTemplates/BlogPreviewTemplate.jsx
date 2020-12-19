985f76b937beb3e2f3bbcf9dd3a5ed638d3705d9
June YAMAMOTO <june@yambal.net>
2020-12-19 23:29:26 +0900

    test

--- src/cms/previewTemplates/BlogPreviewTemplate.jsx
+++ src/cms/previewTemplates/BlogPreviewTemplate.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
-export const BlogPreviewTemplate = props => {
+import PropTypes from 'prop-types'
+
+const BlogPreviewTemplate = ({ entry, widgetFor }) => {
+  const data = entry.getIn(['data']).toJS()
+  const body = widgetFor('body').props.value
 
   return (
     <div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <pre>{JSON.stringify(body, null, 2)}</pre>
       BlogPreviewTemplate
-      <pre>{JSON.stringify(props, null, 2)}</pre>
     </div>
   )
 }
+
+BlogPreviewTemplate.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  widgetFor: PropTypes.func,
+}
+
+export default BlogPreviewTemplate
