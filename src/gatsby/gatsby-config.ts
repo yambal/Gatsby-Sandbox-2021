a9add2bf3e16877d3fd40dc74db74db51e6c9b63
June YAMAMOTO <june@yambal.net>
2020-12-30 03:28:50 +0900

    gatsby-plugin-styled-components

--- src/gatsby/gatsby-config.ts
+++ src/gatsby/gatsby-config.ts
@@ -26,6 +26,7 @@ const gatsbyConfig: GatsbyConfig = {
     'gatsby-plugin-sitemap',
     'gatsby-plugin-svgr',
     'gatsby-transformer-sharp',
+    `gatsby-plugin-styled-components`,
     {
       resolve: 'gatsby-plugin-netlify-cms',
     },
