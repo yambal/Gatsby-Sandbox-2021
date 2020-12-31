fd5d5e416cb18b3dbe8748ec2d2a803a0e836e70
June YAMAMOTO <june@yambal.net>
2020-12-31 11:44:57 +0900

    Query

---
+++ src/pages/etc2/index.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import { graphql, Link, PageProps } from "gatsby"
+import { ETC2IndexQuery } from "../../types/graphql-types"
+import { Box } from '@xstyled/styled-components'
+import { useSiteMetadata } from '../../app/SiteMetadataProvider'
+import { SEO } from '../../components/SEO'
+import { Container } from '../../components/Layout/Container/Container'
+import { PageLayout } from '../../components/page/PageLayout'
+
+type ETC2IndexDataProps = PageProps & {
+  data: ETC2IndexQuery
+}
+
+function ETC2Index(props: ETC2IndexDataProps){
+  const { data: { pageQueryData }, location } = props
+  const siteMetadata = useSiteMetadata()
+
+  return (
+    <PageLayout location={location}>
+      <SEO {...props} pageTitle="砂場" pageKeywords={['テスト', '実験']}/>
+      <Container>
+        <Box as="h1">砂場</Box>
+        {pageQueryData.edges.map(
+          (edge) => {
+            if (edge.node.fields?.slug && edge.node.frontmatter?.title) {
+              return (
+                <Box>
+                  <Link to={edge.node.fields.slug}>{edge.node.frontmatter.title}</Link>
+                  {edge.node.frontmatter.publish_date}
+                </Box>
+              )
+            }
+          }
+        )}
+      </Container>
+    </PageLayout>
+  )
+}
+
+export const pageQuery = graphql`
+  query ETC2Index {
+    pageQueryData:   allMarkdownRemark(filter: {frontmatter: {template: {eq: "ETC2IndexTemplate"}}}, sort: {fields: frontmatter___publish_date, order: DESC}) {
+      edges {
+        node {
+          frontmatter {
+            title
+            publish_date(formatString: "YYYY-MM-DD")
+          }
+          id
+          fields {
+            slug
+          }
+          timeToRead
+        }
+        next {
+          frontmatter {
+            title
+            publish_date(formatString: "YYYY-MM-DD")
+          }
+          fields {
+            slug
+          }
+        }
+        previous {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            publish_date(formatString: "YYYY-MM-DD")
+          }
+        }
+      }
+    }
+  }
+`
+
+export default ETC2Index
